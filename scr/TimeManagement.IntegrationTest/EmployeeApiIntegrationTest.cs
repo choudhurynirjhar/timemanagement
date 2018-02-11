938ef424176d66e4e87e55608d0be584bb44f0c7
Nirjhar <choudhurynirjhar@gmail.com>
2018-02-11 17:59:47 -0500

    Create integration test

---
+++ scr/TimeManagement.IntegrationTest/EmployeeApiIntegrationTest.cs
@@ -0,0 +1,45 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TimeManagement.Data;
+using Xunit;
+
+namespace TimeManagement.IntegrationTest
+{
+    public class EmployeeApiIntegrationTest
+    {
+        [Fact]
+        public async Task Test_Get_All()
+        {
+
+            using (var client = new TestClientProvider().Client)
+            {
+                var response = await client.GetAsync("/api/employee");
+
+                response.EnsureSuccessStatusCode();
+
+                response.StatusCode.Should().Be(HttpStatusCode.OK);
+            }
+        }
+
+        [Fact]
+        public async Task Test_Post()
+        {
+            using (var client = new TestClientProvider().Client)
+            {
+                var response = await client.PostAsync("/api/employee"
+                    , new StringContent(
+                        JsonConvert.SerializeObject(new Employee() { Address = "Test", FirstName = "John", LastName = "Mak", CellPhone = "111-222-3333", HomePhone = "222-333-4444" }), Encoding.UTF8, "application/json"));
+
+                response.EnsureSuccessStatusCode();
+
+                response.StatusCode.Should().Be(HttpStatusCode.OK);
+            }
+        }
+    }
+}
---
+++ scr/TimeManagement.IntegrationTest/TestClientProvider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Net.Http;
+
+namespace TimeManagement.IntegrationTest
+{
+    public class TestClientProvider : IDisposable
+    {
+        private TestServer server;
+
+        public HttpClient Client { get; private set; }
+
+        public TestClientProvider()
+        {
+            server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+
+            Client =  server.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            server?.Dispose();
+            Client?.Dispose();
+        }
+    }
+}
